95d273ae7b21e837ee6ab8e690d2fb2335ca5a99
Leider Calvo <leiderstivencalvogomez@hotmail.com>
2019-05-16 11:20:45 -0500

    ya está en el store

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,46 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 import Listas from '../Listas/Listas';
-import Papa from 'papaparse';
+//import Papa from 'papaparse';
 import store from '../../stores/store';
+import { observer } from 'mobx-react';
 
+@observer
 class App extends Component <any, any> {
   constructor(props: any) {
     super(props);
-    this.state = {
-      data: []
-    };
-
-    this.getData = this.getData.bind(this);
   }
 
   componentWillMount() {
-    this.getCsvData();
-  }
-
-  async getCsvData() {
-    let csvData = await this.fetchCsv();
-    Papa.parse(csvData, {
-        complete: this.getData
-    });
-  }
-
-  fetchCsv() {
-      return fetch('/data/data.csv').then(function (response: any) {
-          let reader = response && response.body.getReader();
-          let decoder = new TextDecoder('utf-8');
-
-          return reader.read().then(function (result: any) {
-            console.log(decoder.decode(result.value));
-              return decoder.decode(result.value);
-          });
-      });
-  }
-
-  getData(result: any) {
-      this.setState({data: result.data});
-      let x = this.state.data[0];
-      {console.log(x[0])}
+    store.loadCsv();
   }
 
   render(){
--- src/stores/Store.ts
+++ src/stores/Store.ts
@@ -8,8 +8,38 @@ export type usuario = String[];
 class Store{
     @observable dataBase : db | null = null;
 
+    constructor() {
+        this.getData = this.getData.bind(this);
+    }
+
     @action setDataBase(db : db){
         this.dataBase = db;
+    }
+
+    loadCsv() {
+        this.getCsvData();
+    }
+    
+    async getCsvData() {
+        let csvData = await this.fetchCsv();
+        Papa.parse(csvData, {
+            complete: this.getData
+        }); 
+    }
+
+    fetchCsv() {
+        return fetch('/data/data.csv').then(function (response: any) {
+            let reader = response && response.body.getReader();
+            let decoder = new TextDecoder('utf-8');
+  
+            return reader.read().then(function (result: any) {
+                return decoder.decode(result.value);
+            });
+        });
+    }
+    
+    getData(result: any) {
+        this.setDataBase(result.data);
     }
 }
 
